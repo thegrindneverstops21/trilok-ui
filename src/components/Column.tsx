b4a14fca7fa7ba8e80669bf40e577f4d59b647ad
thegrindneverstops <Learner@lcx.co.za>
2026-07-14 14:16:59 +0200

    Setup footer column

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,6 +1,15 @@
-interface FooterProps {
+interface ColumnProps {
     title: string;
-    link: string[];
+    links: string[];
 }
 
-export const Column
+export const Column = ({title, links}: ColumnProps) => (
+    <div className="column">
+        <h4 className="column-title">{title}</h4>
+            <ul className="column-list">
+                {links.map((link) => (
+                    <li key={link}><a href="#" className="column-link">{link}</a></li>
+                ))}
+            </ul>
+    </div>
+)
